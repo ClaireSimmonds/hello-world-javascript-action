447fa26053a716b1ff858355a5b83dd92812d522
ClaireSimmonds <1903158+ClaireSimmonds@users.noreply.github.com>
2024-02-08 18:45:15 -0600

    Add updated index

--- dist/index.js
+++ dist/index.js
@@ -28483,22 +28483,51 @@ const github = __nccwpck_require__(5438)
  */
 async function run() {
   try {
-    // The `who-to-greet` input is defined in action metadata file
-    const whoToGreet = core.getInput('who-to-greet', { required: true })
-    core.info(`Hello, ${whoToGreet}!`)
-
-    // Get the current time and set as an output
-    const time = new Date().toTimeString()
-    core.setOutput('time', time)
-
-    // Output the payload for debugging
-    core.info(
-      `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
+    // Set up Github client and context
+    const client = github.getOctokit(
+      core.getInput('repo-token', { required: true })
     )
+    const context = github.context
+
+    if (context.payload.pull_request && context.payload.action === 'opened') {
+      const event_body = getPRSubmittedEvent(context)
+
+      // Output the payload for debugging
+      core.info(`The event payload: ${JSON.stringify(event_body)}`)
+    }
   } catch (error) {
     // Fail the workflow step if an error occurs
     core.setFailed(error.message)
   }
+}
+
+function unixTimestampFromDate(timestring) {
+  return Math.floor(new Date(timestring) / 1000)
+}
+
+function getPRSubmittedEvent(context) {
+  const repo = `${context.payload.organization}/${context.payload.repository}`
+  const github_user = `${context.payload.sender.login}`
+
+  const event = {
+    alert_type: 'info',
+    date_happened: unixTimestampFromDate(
+      context.payload.pull_request.created_at
+    ),
+    priority: 'normal',
+    text: `"${context.payload.changed_files} files changed by ${github_user} with ${context.payload.additions} additions and ${context.payload.deletions} deletions."`,
+    title: `"%%%[Pull Request #${context.payload.number}](${context.payload.pull_request.url}) opened in ${repo}: ${context.payload.pull_request.title}%%%"`,
+    tags: [
+      'metric:contributor_activity',
+      'event_type:pull_request',
+      'action:opened',
+      `"draft:${context.payload.pull_request.draft}"`,
+      `"repo:${repo}"`,
+      `"actor:${github_user}"`
+    ]
+  }
+
+  return event
 }
 
 module.exports = {
